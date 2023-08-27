61f6f249220c72e00b5ca04cefdafa01aea7f8a6
Moneshsoni <sonimonesh115@gmail.com>
2023-08-27 22:34:53 +0530

    add the model validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
-  validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :title, presence: { message: "You need to provide the value of title" }
+  # validates :body, presence: true, length: { minimum: 10 }
 end
